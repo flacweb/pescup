c87a77439740e58efcb11a2e3dbde4d720809ac8
flac <julien@pwlk.fr>
2013-11-18 11:29:22 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pescup::Application.config.secret_key_base = 'eb31c34fda180bd5da8bb04f1800960fbf8268cae51377acb0fae0ad6d8e62650ac5f173ab2bf93c9af57da50e851658c756f940c801f38b4d6abe09931c2913'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pescup::Application.config.session_store :cookie_store, key: '_pescup_session'
